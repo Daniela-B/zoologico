325e9325fbbc16c415632aa0523f807ba20ac447
Daniela-B <61893130+Daniela-B@users.noreply.github.com>
2020-03-31 09:28:51 -0600

    Update zoo.py

--- zoo.py
+++ zoo.py
@@ -12,18 +12,32 @@ class Animal:
             self.nombre=input("Dime el nombre del animal: ")
             lista.append(self.nombre)
             self.tipo =input("Dime el tipo de animal: ")
+            self.set_clasificacion()
             print("Nuevo animal creado")
         print (lista)
-        ID= int(input("Dime un id, 0 es el primero en la lista: "))
-        dato =lista[ID]
-        print ("El nombre de este animal es "+ dato)
+        self.get_posicion()
         self.set_nuevotipo()
-        
+        self.get_nuevotipo()
+    
+    
+    
+    
     def set_nuevotipo(self):
         self.tipo= input("Dime el nuevo tipo: ")
         print ("El nuevo tipo es "+ self.tipo)
-
+    def set_clasificacion(self):
+        self.clasificacion = input("Dime la clasificacin del animal: ")
+    def get_posicion (self):
+        ID= int(input("Dime un id, 0 es el primero en la lista: "))
+        self.name = lista[ID]
+        print("El nombre de este animal es: " + self.name)    
+    def get_nuevotipo(self):
+        print ("El nombre del animal es " + self.name)
+        print ("El tipo actual del animal es "+ self.tipo)
+    
+        
 animal = Animal ()
+
 
 
 
